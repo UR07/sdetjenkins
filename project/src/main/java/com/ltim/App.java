6db09ca1c243329798f5187b891e3ec2055011e3
neojarvis <techadmin@iamneo.ai>
2025-03-27 13:14:02 +0000

    Auto-save-commit-3/27/2025--6:43:56-PM

---
+++ project/src/main/java/com/ltim/App.java
@@ -0,0 +1,13 @@
+package com.ltim;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
